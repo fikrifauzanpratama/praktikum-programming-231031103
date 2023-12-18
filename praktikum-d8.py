2754169fd809d8208a533262acc945b33a150d78
fikrifauzanpratama <153707350+fikrifauzanpratama@users.noreply.github.com>
2023-12-18 16:58:10 +0700

    Create praktikum-d8.py

---
+++ praktikum-d8.py
@@ -0,0 +1,30 @@
+pwd_benar = 'sid'
+pwd_benar2 = 'SI23D'
+pwd_benar3 = 'sistemd'
+
+def cekpass(id_password, password, page):
+    limit = 3
+    i = 0
+    while i < limit:
+        pwd = input(f'Masukkan Password {id_password} untuk Halaman {page}= ')
+        if pwd == password:  
+            print(f'Password Benar, Selamat Datang di Halaman {page}')
+            return True
+        else:
+            i += 1
+            if i == limit:
+                print('Kesempatan Anda Habis!')
+                return False
+            else:
+                print(f'Password Salah. Kesempatan anda tersisa {limit - i} kali')
+
+# Tes password pertama
+tes1 = cekpass('pertama', 'sid', 1)
+if tes1:
+    # Tes password kedua
+    tes2 = cekpass('kedua', 'SI23D', 2)
+    if tes2:
+        # Tes password ketiga
+        tes3 = cekpass('ketiga', 'sistemd', 3)
+        if tes3:
+            print('Selamat Anda Berhasil Login')
