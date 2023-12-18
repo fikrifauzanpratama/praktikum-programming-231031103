6b5f7ab5a2a49b81ccbda6f3a2d9e16ed7360a84
fikrifauzanpratama <153707350+fikrifauzanpratama@users.noreply.github.com>
2023-12-18 17:00:20 +0700

    Create tugas 10-1.py

---
+++ tugas 10-1.py
@@ -0,0 +1,9 @@
+
+Nim = 231031103
+
+bilangan = int(input("Masukkan bilangan: "))
+               
+if bilangan % 3 == 0:
+    print("Bilangan", bilangan, "adalah bilangan genap.")
+else:
+    print("Bilangan", bilangan, "adalah bilangan ganjil.")
